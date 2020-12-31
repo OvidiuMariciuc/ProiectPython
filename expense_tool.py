bd42c480c187c2452e8b25f177e3238236aa19dc
OvidiuMariciuc <ovidiumariciuc99@gmail.com>
2020-12-31 14:16:11 +0200

    Monitorizare Director + Loguri operatiuni + Diverse

--- expense_tool.py
+++ expense_tool.py
@@ -11,9 +11,18 @@ def citirePraguri(fisierPraguri):
     """
     functia care adauga pragurile din fisierul text intr-un dictionar
     """
-    with open(fisierPraguri) as json_file:
+    with open(fisierPraguri, "r", encoding='utf-8') as json_file:
         praguri_data = json.load(json_file)
     return praguri_data
+
+
+def citireFactura(fisierFactura):
+    """
+    functia care adauga pragurile din fisierul text intr-un dictionar
+    """
+    with open(fisierFactura, "r", encoding='utf-8') as json_file:
+        factura_data = json.load(json_file)
+    return factura_data
 
 
 # print(praguri_data["Marketing"])
@@ -47,7 +56,11 @@ class Handler(FileSystemEventHandler):
 
         elif event.event_type == 'created':
             # Take any action here when a file is first created.
-            print("Received created event - %s." % event.src_path)
+            print("Am primit o factura noua - %s." % event.src_path)
+            time.sleep(3)
+            factura = citireFactura(event.src_path)
+            adaugareFactura(factura)
+            verifDepasire(dictionar_praguri)
 
 
 # stabilirea conexiunii cu baza de date
@@ -65,6 +78,8 @@ def adaugareFactura(factura_noua):
         print(data_factura)
 
         postgres_insert_query = """ INSERT INTO factura (furnizor, tip, Data, categorie, valoare) VALUES (%s,%s,%s,%s,%s)"""
+        if factura_noua["Categorie"] == "":
+            factura_noua["Categorie"] = "Diverse"
         record_to_insert = (
             factura_noua["Furnizor"], factura_noua["Tip"], data_factura, factura_noua["Categorie"],
             factura_noua["Valoare"])
@@ -72,11 +87,11 @@ def adaugareFactura(factura_noua):
 
         conn.commit()
         count = cursor.rowcount
-        print(count, "Inregistrare adaugat cu succes in tabela factura")
+        print(count, "Inregistrare adaugata cu succes in tabela factura")
 
     except (Exception, psycopg2.Error) as error:
         if (conn):
-            print("Inregistrarea nu a fost adaugat in tabela factura", error)
+            print("Inregistrarea nu a fost adaugata in tabela factura", error)
 
 
 def verifDepasire(dictionar_praguri):
@@ -92,7 +107,8 @@ def verifDepasire(dictionar_praguri):
         suma = suma + row[0]
     # print("Suma totala obtinuta din factorile din categoria 'Marketing' este",suma)
     if suma > dictionar_praguri['Marketing']:
-        print("Alerta")
+        print("Atentie, bugetul alocat pentru categoria 'Marketing' a fost depasit cu",
+              suma - dictionar_praguri['Marketing'], "RON.")
 
     cursor.execute(""" SELECT valoare FROM factura where categorie='Salarii' """)
 
@@ -102,7 +118,8 @@ def verifDepasire(dictionar_praguri):
         suma = suma + row[0]
     # print("Suma totala obtinuta din factorile din categoria 'Salarii' este",suma)
     if suma > dictionar_praguri['Salarii']:
-        print("Alerta")
+        print("Atentie, bugetul alocat pentru categoria 'Salarii' a fost depasit cu",
+              suma - dictionar_praguri['Salarii'], "RON.")
 
     cursor.execute(""" SELECT valoare FROM factura where categorie='Tehnologie' """)
 
@@ -112,7 +129,8 @@ def verifDepasire(dictionar_praguri):
         suma = suma + row[0]
     # print("Suma totala obtinuta din factorile din categoria 'Tehnologie' este",suma)
     if suma > dictionar_praguri['Tehnologie']:
-        print("Alerta")
+        print("Atentie, bugetul alocat pentru categoria 'Tehnologie' a fost depasit cu",
+              suma - dictionar_praguri['Tehnologie'], "RON.")
 
     cursor.execute(""" SELECT valoare FROM factura where categorie='Echipamente' """)
 
@@ -122,7 +140,8 @@ def verifDepasire(dictionar_praguri):
         suma = suma + row[0]
     # print("Suma totala obtinuta din factorile din categoria 'Echipamente' este",suma)
     if suma > dictionar_praguri['Echipamente']:
-        print("Alerta")
+        print("Atentie, bugetul alocat pentru categoria 'Echipamente' a fost depasit cu",
+              suma - dictionar_praguri['Echipamente'], "RON.")
 
     cursor.execute(""" SELECT valoare FROM factura where categorie='Transport' """)
 
@@ -132,7 +151,8 @@ def verifDepasire(dictionar_praguri):
         suma = suma + row[0]
     # print("Suma totala obtinuta din factorile din categoria 'Transport' este",suma)
     if suma > dictionar_praguri['Transport']:
-        print("Alerta")
+        print("Atentie, bugetul alocat pentru categoria 'Transport' a fost depasit cu",
+              suma - dictionar_praguri['Transport'], "RON.")
 
     cursor.execute(""" SELECT valoare FROM factura where categorie='Chirie' """)
 
@@ -142,7 +162,8 @@ def verifDepasire(dictionar_praguri):
         suma = suma + row[0]
     # print("Suma totala obtinuta din factorile din categoria 'Chirie' este",suma)
     if suma > dictionar_praguri['Chirie']:
-        print("Alerta")
+        print("Atentie, bugetul alocat pentru categoria 'Chirie' a fost depasit cu",
+              suma - dictionar_praguri['Chirie'], "RON.")
 
     cursor.execute(""" SELECT valoare FROM factura where categorie='Utilitati' """)
 
@@ -150,9 +171,10 @@ def verifDepasire(dictionar_praguri):
     suma = 0
     for row in data:
         suma = suma + row[0]
-    print("Suma totala obtinuta din factorile din categoria 'Utilitati' este",suma)
+    print("Suma totala obtinuta din factorile din categoria 'Utilitati' este", suma)
     if suma > dictionar_praguri['Utilitati']:
-        print("Alerta")
+        print("Atentie, bugetul alocat pentru categoria 'Utilitati' a fost depasit cu",
+              suma - dictionar_praguri['Utilitati'], "RON.")
 
     cursor.execute(""" SELECT valoare FROM factura where categorie='Consumabile' """)
 
@@ -162,7 +184,8 @@ def verifDepasire(dictionar_praguri):
         suma = suma + row[0]
     # print("Suma totala obtinuta din factorile din categoria 'Consumabile' este",suma)
     if suma > dictionar_praguri['Consumabile']:
-        print("Alerta")
+        print("Atentie, bugetul alocat pentru categoria 'Consumabile' a fost depasit cu",
+              suma - dictionar_praguri['Consumabile'], "RON.")
 
     cursor.execute(""" SELECT valoare FROM factura where categorie='Calatorii' """)
 
@@ -172,7 +195,8 @@ def verifDepasire(dictionar_praguri):
         suma = suma + row[0]
     # print("Suma totala obtinuta din factorile din categoria 'Calatorii' este",suma)
     if suma > dictionar_praguri['Calatorii']:
-        print("Alerta")
+        print("Atentie, bugetul alocat pentru categoria 'Calatorii' a fost depasit cu",
+              suma - dictionar_praguri['Calatorii'], "RON.")
 
     cursor.execute(""" SELECT valoare FROM factura where categorie='Diverse' """)
 
@@ -182,12 +206,36 @@ def verifDepasire(dictionar_praguri):
         suma = suma + row[0]
     # print("Suma totala obtinuta din factorile din categoria 'Diverse' este",suma)
     if suma > dictionar_praguri['Diverse']:
-        print("Alerta")
+        print("Atentie, bugetul alocat pentru categoria 'Diverse' a fost depasit cu",
+              suma - dictionar_praguri['Diverse'], "RON.")
+
+
+def afisareIstoric():
+    """
+    functia care va afisa istoricul facturilor primite
+    """
+    print("\n< Istoric facturi >")
+    cursor.execute(""" SELECT categorie, valoare, data FROM factura """)
+
+    data = cursor.fetchall()
+    print(
+        "__________________________________________________________________________________________________")
+
+    for row in data:
+        print("A fost inregistrata o factura din categoria:", row[0], "in valoare de", row[1], "RON la data", row[2],
+              ";")
+    print(
+        "___________________________________________________________________________________________________")
+
 
 dictionar_praguri = citirePraguri("PraguriCategorii.txt")
-verifDepasire(dictionar_praguri)
-exfactura = {"Furnizor": "DelGaz", "Tip": "Factura Gaz", "Data": "27/09/2020", "Categorie": "Utilitati", "Valoare": 350}
+# verifDepasire(dictionar_praguri)
+# exfactura = {"Furnizor": "DelGaz", "Tip": "Factura Gaz", "Data": "27/09/2020", "Categorie": "", "Valoare": 1050}
 # adaugareFactura(exfactura)
+afisareIstoric()
+
+watcher = Watcher()
+watcher.run()
 
 # inchidem conexiunea cu baza de date.
 if (conn):
